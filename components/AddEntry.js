8673cd54376f07765499738fa36553b7229aa033
Alireza Ebadighajari <alirezaebg@gmail.com>
2020-11-04 18:58:04 -0800

    Add AsyncStorage via api.js to submit and reset methods in AddEntry

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -6,6 +6,7 @@ import UdaciSteppers from './UdaciSteppers'
 import DateHeader from './DateHeader'
 import { Ionicons } from '@expo/vector-icons'
 import TextButton from './TextButton'
+import { submitEntry, removeEntry } from '../utils/api'
 
 //SubmitBtn is a functional component
 function SubmitBtn({ onPress }) {
@@ -75,6 +76,7 @@ export default class AddEntry extends Component {
         // Navigate to home
 
         // Save to 'DB'
+        submitEntry({ key, entry })
 
         // Clear local notification
     }
@@ -87,6 +89,7 @@ export default class AddEntry extends Component {
         // Route to Home
 
         // Update "DB"
+        removeEntry(key)
     }
 
     render() {
