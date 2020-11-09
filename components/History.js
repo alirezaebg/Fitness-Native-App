5f0a349ee5aaf0647f080999daebc4d06cedefad
Alireza Ebadighajari <alirezaebg@gmail.com>
2020-11-08 21:10:36 -0800

    Add ready status and AppLoading

--- components/History.js
+++ components/History.js
@@ -8,8 +8,12 @@ import UdaciFitnessCalendar from 'udacifitness-calendar'
 import { white } from '../utils/colors'
 import DateHeader from './DateHeader'
 import MetricCard from './MetricCard'
+import { AppLoading } from 'expo'
 
 class History extends Component {
+    state = {
+        ready: false,
+    }
     componentDidMount() {
         const { dispatch } = this.props
 
@@ -58,6 +62,11 @@ class History extends Component {
 
     render() {
         const { entries } = this.props
+        const { ready } = this.state
+
+        if (ready === false) {
+            return <AppLoading />
+        }
 
         return (
             <UdaciFitnessCalendar
@@ -71,27 +80,27 @@ class History extends Component {
 
 const styles = StyleSheet.create({
     item: {
-      backgroundColor: white,
-      borderRadius: Platform.OS === 'ios' ? 16 : 2,
-      padding: 20,
-      marginLeft: 10,
-      marginRight: 10,
-      marginTop: 17,
-      justifyContent: 'center',
-      shadowRadius: 3,
-      shadowOpacity: 0.8,
-      shadowColor: 'rgba(0, 0, 0, 0.24)',
-      shadowOffset: {
-        width: 0,
-        height: 3
-      },
+        backgroundColor: white,
+        borderRadius: Platform.OS === 'ios' ? 16 : 2,
+        padding: 20,
+        marginLeft: 10,
+        marginRight: 10,
+        marginTop: 17,
+        justifyContent: 'center',
+        shadowRadius: 3,
+        shadowOpacity: 0.8,
+        shadowColor: 'rgba(0, 0, 0, 0.24)',
+        shadowOffset: {
+            width: 0,
+            height: 3
+        },
     },
     noDataText: {
-      fontSize: 20,
-      paddingTop: 20,
-      paddingBottom: 20
+        fontSize: 20,
+        paddingTop: 20,
+        paddingBottom: 20
     }
-  })
+})
 
 function mapStateToProps(entries) {
     return {
