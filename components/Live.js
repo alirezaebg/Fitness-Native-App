710a0f3236e0bc00a827618b575f24538cc7ed69
Alireza Ebadighajari <alirezaebg@gmail.com>
2020-11-15 14:46:38 -0800

    Add Animated API

--- components/Live.js
+++ components/Live.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, Text, ActivityIndicator, TouchableOpacity, StyleSheet } from 'react-native'
+import { View, Text, ActivityIndicator, TouchableOpacity, StyleSheet, Animated } from 'react-native'
 import { Foundation } from '@expo/vector-icons'
 import { purple, white } from '../utils/colors'
 import * as Permissions from 'expo-permissions'
@@ -12,6 +12,7 @@ export default class Live extends Component {
         coords: null,
         status: null,
         direction: '',
+        bounceValue: new Animated.Value(1),
     }
 
     componentDidMount() {
@@ -50,6 +51,14 @@ export default class Live extends Component {
         }, ({ coords }) => {
             const newDirection = calculateDirection(coords.heading)
             const { direction, bounceValue } = this.state
+
+            if (newDirection !== direction) {
+                Animated.sequence([
+                    Animated.timing(bounceValue, { duration: 200, toValue: 1.04 }),
+                    Animated.spring(bounceValue, { toValue: 1, friction: 4 })
+                ]).start()
+            }
+
             this.setState(() => ({
                 coords,
                 status: 'granted',
@@ -59,7 +68,7 @@ export default class Live extends Component {
     }
 
     render() {
-        const { status, coords, direction } = this.state
+        const { status, coords, direction, bounceValue } = this.state
 
         if (status === null) {
             return <ActivityIndicator style={{ marginTop: 30 }} />
@@ -93,9 +102,10 @@ export default class Live extends Component {
             <View style={styles.container}>
                 <View style={styles.directionContainer}>
                     <Text style={styles.header}>You're heading</Text>
-                    <Text style={styles.direction}>
+                    <Animated.Text
+                        style={[styles.direction, { transform: [{ scale: bounceValue }] }]}>
                         {direction}
-                    </Text>
+                    </Animated.Text>
                 </View>
                 <View style={styles.metricContainer}>
                     <View style={styles.metric}>
@@ -175,5 +185,5 @@ const styles = StyleSheet.create({
         fontSize: 25,
         textAlign: 'center',
         marginTop: 5,
-    },
+    }
 })
